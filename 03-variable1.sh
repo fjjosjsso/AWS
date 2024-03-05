f8774df13f45550e2394208efe078a257563fa59
fjjosjsso <sakaraysairam1996@gmail.com>
2024-03-05 12:39:42 +0530

    conversation commit

---
+++ 03-variable1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$1: Hello $2 Good morning, How are you"
+echo "$2: Good morning $1. How are you"
+echo "$1: I am good, what about you"
+echo "$2: Thanks for asking, I am also good $1"
